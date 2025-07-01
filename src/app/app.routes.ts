4c85ba4bfaa701010fa6154f77b4b8b68a495183
IvoGarraza <ivogarrachip@gmail.com>
2025-07-01 14:16:36 -0300

    estructurado y creacion de navbar y home

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Layout } from './components/layout/layout';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: Layout,
+    children: [
+      { path: '', component: Home },
+    ]
+  }
+];
---
+++ src/app/components/layout/layout.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Navbar } from "../navbar/navbar";
+import { Home } from "../../pages/home/home";
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-layout',
+  imports: [Navbar, RouterOutlet],
+  templateUrl: './layout.html',
+  styleUrl: './layout.css'
+})
+export class Layout {
+
+}
